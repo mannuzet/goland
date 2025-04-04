4fb5b7e4a67bc4f6d583f094ff1cbf6fee2498fe
mannuzet <emanuelazatinferreira@gmail.com>
2025-04-04 08:22:49 -0300

    Stand Library

--- main.go
+++ main.go
@@ -1,38 +1,27 @@
 package main
 
-import "fmt"
+import (
+"fmt"  
+"strings"
+ "sort"
+)
 
 func main() {
-    var ages = [4] int{17, 16, 20, 40}
-    names := [4] string{"Manu", "Belly", "Isa", "Ju"}
-
+	greeting := "Hello my friends"
+    fmt.Println(strings.Contains(greeting, "friends"))
+    fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
+    fmt.Println(strings.ToUpper(greeting))
+    fmt.Println(strings.ToLower(greeting))
+    fmt.Println(strings.Index(greeting, "my"))
+    fmt.Println(strings.Split(greeting, "  "))
+    ages := []int {50, 80, 10}
+    sort.Ints(ages)
     fmt.Println(ages)
+    index := sort.SearchInts(ages, 50)
+    fmt.Println(index)
+    names := []string {"Sabrina", "Olivia", "Lana"}
+    sort.Strings(names)
     fmt.Println(names)
-
-   names[0] = "Manuzett" 
-   fmt.Println(names)
-
-   // Slice
-   var score = []int{100, 200, 300, 400}
-   fmt.Println(score)
-
-   score [1] = 2
-   fmt.Println(score, len(score), cap(score))
-
-   rangeOne := score[1:3]
-   fmt.Println(rangeOne)
-
-   rangeTwo := score[2:]
-   fmt.Println(rangeTwo)
-
-   rangeThree := score[:3]
-   fmt.Println(rangeThree)
-
-   var superheros = []string{"Deadpool", "Spider-man", "Batman"}
-   fmt.Println(superheros)
-
-   superheros = append(superheros, "Ironman", "Thor")
-   fmt.Println(superheros, len(superheros), cap(superheros))
- 
-
+    fmt.Println(sort.SearchStrings(names, "Sabrina"))
+   
 }
