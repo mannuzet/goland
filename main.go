632fb277f9433d85a943b6c97f72d38b3b8312af
mannuzet <emanuelazatinferreira@gmail.com>
2025-04-10 08:01:29 -0300

    vetor 10.04

--- main.go
+++ main.go
@@ -1,53 +1,22 @@
 package main
 
 import (
-"fmt"  
-"strings"
-"sort"
+	"fmt"
 )
 
 func main() {
-	greeting := "Hello my friends"
-    fmt.Println(strings.Contains(greeting, "friends"))
-    fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-    fmt.Println(strings.ToUpper(greeting))
-    fmt.Println(strings.ToLower(greeting))
-    fmt.Println(strings.Index(greeting, "my"))
-    fmt.Println(strings.Split(greeting, "  "))
-    ages := []int {50, 80, 10}
-    sort.Ints(ages)
-    fmt.Println(ages)
-    index := sort.SearchInts(ages, 50)
-    fmt.Println(index)
-    names := []string {"Sabrina", "Olivia", "Lana"}
-    sort.Strings(names)
-    fmt.Println(names)
-    fmt.Println(sort.SearchStrings(names, "Sabrina"))
-   x :=0
+	var numbers [5]int
+	var sum int
 
-   for x < 5 {
-   fmt.Println(x)
-   x++
-}
+	// Slice
+	fmt.Println("Type five numbers:")
 
-for i:=0; i < 5; i++{
-    fmt.Println("for 2:", i)
-}
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Number %d: ", i+1)
+		fmt.Scanln(&numbers[i])
+		sum += numbers[i]
+	}
 
-for i:=0; i <len(names); i++{
-    fmt.Println(names[i])
-}
-for index, value := range names{
-    fmt.Println("the index is:", index, "and the value is:", value)
-}
-
-for index, value := range ages{
-    fmt.Println("the index is:", index, "and the value is:", value)
-}
-
-superheros := []string{"Deadpool", "Spider-man", "Batman"}
-
-for index, value := range superheros{
-    fmt.Println("The hero number is:", index, "the hero name is:", value)
-}
+	// Slice
+	fmt.Printf("The total sum is: %d\n", sum)
 }
