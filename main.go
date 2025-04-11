583bd2d796dc2ec4e7181a53441fdc8ff6d33a0b
mannuzet <emanuelazatinferreira@gmail.com>
2025-04-11 13:26:59 -0300

    função

--- main.go
+++ main.go
@@ -4,42 +4,16 @@ import (
     "fmt"
 )
 
+func sayGreeting(name string){
+fmt.Println("Hii", name)
+}
+
+func addNumber(number1 int, number2 int) int{
+    return number1 + number2
+}
 func main() {
+sayGreeting("Sabrina")
+result := addNumber(10, 20)
+fmt.Println(result)
 
-    var saldo float64
-
-    fmt.Print("Digite o saldo inicial: R$ ")
-    fmt.Scan(&saldo)
-
-    for {
-        var comando string
-        var valor float64
-
-        fmt.Printf("\nSaldo atual: R$ %.2f\n", saldo)
-
-        fmt.Print("\nDigite o comando (saque, deposito ou sair): ")
-        fmt.Scan(&comando)
-
-        if comando == "sair" {
-            fmt.Println("Saindo do programa...")
-            break
-        }
-
-        fmt.Print("Digite o valor: R$ ")
-        fmt.Scan(&valor)
-
-        if comando == "saque" {
-            if valor > saldo {
-                fmt.Println("Saldo insuficiente para realizar o saque.")
-            } else {
-                saldo -= valor
-                fmt.Printf("Saque de R$ %.2f realizado com sucesso.\n", valor)
-            }
-        } else if comando == "deposito" {
-            saldo += valor
-            fmt.Printf("Depósito de R$ %.2f realizado com sucesso.\n", valor)
-        } else {
-            fmt.Println("Comando inválido. Tente novamente.")
-        }
-    }
 }
