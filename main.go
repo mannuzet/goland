56ce70e54d864b54939b07c1506a52c5dcc60190
mannuzet <emanuelazatinferreira@gmail.com>
2025-04-11 13:50:28 -0300

    atividade saldo em função

--- main.go
+++ main.go
@@ -4,16 +4,50 @@ import (
     "fmt"
 )
 
-func sayGreeting(name string){
-fmt.Println("Hii", name)
-}
-
-func addNumber(number1 int, number2 int) int{
-    return number1 + number2
-}
 func main() {
-sayGreeting("Sabrina")
-result := addNumber(10, 20)
-fmt.Println(result)
+    var balance float32
+    fmt.Println("Insira o valor inicial:")
+    fmt.Scan(&balance)
+    escolherOperacao(&balance)
+}
 
+func escolherOperacao(balance *float32) {
+    var option int
+    fmt.Println("Do you want to withdraw (1), deposit (2) or close the session (3)?")
+    fmt.Scan(&option)
+    switch option{ 
+ 
+    case 1:
+        sacar(balance)
+    case 2:
+        depositar(balance)
+    case 3:
+        encerrar()
+    default:
+        fmt.Println("Invalid option")
+    }
+}
+
+func depositar(balance *float32) {
+    var value float32
+    fmt.Println("what amount do you want to deposit?")
+    fmt.Scan(&value)
+    *balance += value
+    fmt.Println("Seu saldo atual é:", *balance)
+}
+
+func sacar(balance *float32) {
+    var value float32
+    fmt.Println("How much do you want to withdraw?")
+    fmt.Scan(&value)
+    if value > *balance {
+        fmt.Println("Saldo insuficiente.")
+    } else {
+        *balance -= value
+        fmt.Println("Seu saldo atual é:", *balance)
+    }
+}
+
+func encerrar() {
+    fmt.Println("Session closed")
 }
