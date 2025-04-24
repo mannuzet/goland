8f94df6a18ddeb65e58bff0e57825f82022f9486
mannuzet <emanuelazatinferreira@gmail.com>
2025-04-24 14:19:13 -0300

    siglas

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
-import "fmt"
+import  "fmt";
 
-func analyzeNotes(note1, note2 float64) (float64, string) {
-	media := (note1 + note2) / 2
-
-	var text string
-	if media <= 6 {
-		text = "you are failed"
-	} else if media >= 7 {
-		text = "you are approved"
-	} else {
-		text = "in recovery"
-	}
-	return media, text
+func main(){
+capitais := map[string]string{
+    "SP" : "São Paulo",
+    "RJ" : "Rio de Janeiro",
+    "ES" : "Espírito Santo",
+    "AC" : "Acre",
 }
+capitais["BH"] = "Belo Horizonte"
 
-func main() {
-	average, result := analyzeNotes(7.5, 7.5)
-	fmt.Println("average", average)
-	fmt.Println("result", result)
+for k,v := range capitais {
+    fmt.Println("Sigla, Nome", k, v)
+}
+delete(capitais, "AC")
+
+for k,v := range capitais {
+    fmt.Println("Sigla, Nome", k, v)
+}
 }
